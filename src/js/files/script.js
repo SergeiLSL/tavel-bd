673bbd847e710178703a11c8d732ab0b69636fa0
Sergei Student <lsl1959@mail.ru>
2023-05-19 20:39:35 +0600

    finish header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,39 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+/* ++++++++++++++++++++++++++++++ */
+// ? БУРГЕР
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+   document.addEventListener('click', documentActions);
+}
+
+
+function documentActions(e) {
+   const targetElement = e.target;
+
+   if (targetElement.closest(".icon-menu")) {
+      document.documentElement.classList.toggle("menu-open");
+   }
+
+   if (targetElement.closest("[data-goto]")) {
+      document.documentElement.classList.contains("menu-open")
+         ? document.documentElement.classList.remove("menu-open")
+         : null;
+
+      const goTo = targetElement.closest("[data-goto]").dataset.goto;
+      const goToElement = document.querySelector(goTo);
+      const headerHeight = document.querySelector(".header").offsetHeight;
+
+      if (goToElement) {
+         window.scrollTo({
+            top: goToElement.offsetTop - (headerHeight + 15),
+            behavior: "smooth",
+         });
+      }
+      e.preventDefault();
+   }
+}
